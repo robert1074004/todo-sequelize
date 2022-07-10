1c531ce205043b324ee275231a931166e2016ce6
robert1074004 <abc50113@yahoo.com.tw>
2022-07-10 20:24:14 +0800

    feat: add login function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 const app = express()
 const PORT = 3000
 const db = require('./models')
@@ -9,8 +12,18 @@ const Todo = db.Todo
 const User = db.User
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
 app.set('view engine','hbs')
+app.use(session({
+    secret:'ThisIsMySecret',
+    resave:false,
+    saveUninitialized:true
+}))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+
+
+
+
+usePassport(app)
 
 app.get('/',(req,res) => {
     return Todo.findAll({
@@ -32,9 +45,10 @@ app.get('/users/login',(req,res) => {
     res.render('login')
 })
 
-app.post('/users/login',(req,res) => {
-    res.send('login')
-})
+app.post('/users/login', passport.authenticate('local',{
+    successRedirect:'/',
+    failureRedirect:'/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
     res.render('register')
